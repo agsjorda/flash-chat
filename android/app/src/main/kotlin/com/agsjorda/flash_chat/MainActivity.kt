ece03cff9b2db7ede896290f6201f13b2491bdf3
agsjorda <agsjorda@gmail.com>
2023-10-21 00:13:50 +0800

    added welcomem, login, registration and chat screens

---
+++ android/app/src/main/kotlin/com/agsjorda/flash_chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.agsjorda.flash_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:flash_chat/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+class FlashChat extends StatelessWidget {
+  const FlashChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+          textTheme:
+              const TextTheme(bodyMedium: TextStyle(color: Colors.black54))),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        'login_screen': (context) => LoginScreen(),
+        'registration_screen': (context) => RegistrationScreen(),
+        'chat_screen': (context) => ChatScreen(),
+      },
+    );
+  }
+}
+
+void main() {
+  runApp(const FlashChat());
+}
